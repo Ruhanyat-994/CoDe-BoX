a96f4b31eb26dce43bcdc4e4476a7e83a5d68a39
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-03-09 22:20:24 +0600

    Automated commit for programming in .

--- Ruhanyats_code/pushcut.sh
+++ Ruhanyats_code/pushcut.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+# Function to process each directory
+process_directory() {
+    local directory="$1"
 
-git status
-git add *.cpp
-git commit -m "Automated commit for programming"
+    # Move to the directory
+    cd "$directory" || exit
+
+    # Print the current directory
+    echo "Current directory: $directory"
+
+    # Add all changes
+    git add .
+
+    # Commit the changes
+    git commit -m "Automated commit for programming in $directory"
+
+    # Move back to the original directory
+    cd - || exit
+}
+
+# Get the list of all directories (excluding .git)
+directories=$(find . -type d -not -path "./.git*")
+
+# Iterate through the list and process each directory
+for dir in $directories; do
+    process_directory "$dir"
+done
+
+# Push changes to the remote repository
 git push -u origin dev
-
