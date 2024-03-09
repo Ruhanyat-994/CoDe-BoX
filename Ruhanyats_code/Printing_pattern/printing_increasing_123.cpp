df1ae3d0d07b192e7a37f8d0cbc8c86105f4f90a
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-03-09 23:15:26 +0600

    Automated commit for programming in ./Printing_pattern

---
+++ Ruhanyats_code/Printing_pattern/printing_increasing_123.cpp
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n ;
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++)
+    {
+        for(int col=1;col<=row;col++)
+        {
+            printf("%d ",col);
+        }
+        printf("\n");
+    }
+}
+
