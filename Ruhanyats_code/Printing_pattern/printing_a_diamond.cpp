01d67ea1c762f98c0bbf2520c4bca0f0a7adb66a
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-03-10 16:17:29 +0600

    Automated commit for programming in ./Printing_pattern

--- Ruhanyats_code/Printing_pattern/printing_a_diamond.cpp
+++ Ruhanyats_code/Printing_pattern/printing_a_diamond.cpp
@@ -1,1 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n ;
+    scanf("%d",&n);
+    for(int row=1;row<=n*2-1;row++)
+    {
+        int noOfCol = (row>n) ? (2*n -row): row;
+        int noOfSpaces=n-noOfCol;
+
+        for(int s=1;s<=noOfSpaces;s++)
+        {
+            printf(" ");
+        }
+        for(int col=1;col<=noOfCol;col++)
+        {
+
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
 
