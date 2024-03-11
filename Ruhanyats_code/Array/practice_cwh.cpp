c96d8efd9dadba1f5038cf0400cf3723688eb2de
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-03-11 11:48:14 +0600

    Automated commit for programming in ./Array

---
+++ Ruhanyats_code/Array/practice_cwh.cpp
@@ -0,0 +1,58 @@
+
+/*#include<stdio.h>
+int main()
+{
+   int ages[]={20,23,12,56,23,45,89};
+   float avg,sum=0;
+   int length = sizeof(ages)/sizeof(ages[0]);
+
+   for (int i=0;i<length;i++)
+   {
+       sum+=ages[i];
+
+   }
+   avg = sum/length;
+   printf("The average is :%.2f",avg);
+}
+
+
+#include<stdio.h>
+int main()
+{
+    int ages[]={20,23,12,56,23,45,89,18};
+    int length = sizeof(ages)/sizeof(ages[0]);
+
+    int lowestAge= ages[0];
+
+    for(int i=0; i<length;i++)
+    {
+        if(lowestAge > ages[i])
+        {
+            lowestAge=ages[i];
+        }
+    }
+    printf("%d",lowestAge);
+}
+
+
+#include<stdio.h>
+int main()
+{
+    int matrix[2][3]={{1,2,3},{4,5,6}};
+
+    matrix[1][2]=9;
+
+    for(int i =0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d\n",matrix[i][j]);
+        }
+    }
+
+
+
+}
+*/
+
+
