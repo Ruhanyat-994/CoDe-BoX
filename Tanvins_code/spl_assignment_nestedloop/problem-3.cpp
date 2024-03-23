f9b610f6103f6e1f4b2c8f10ee79dafe5c6a6f51
Tanvin-624 <159693541+Tanvin-624@users.noreply.github.com>
2024-03-23 16:39:20 +0600

    Automated commit for programming in ./spl_assignment_nestedloop

--- Tanvins_code/spl_assignment_nestedloop/problem-3.cpp
+++ Tanvins_code/spl_assignment_nestedloop/problem-3.cpp
@@ -1,4 +1,5 @@
 
+
 #include<stdio.h>
 int main()
 {
@@ -6,7 +7,7 @@ int main()
     scanf("%d",&num);
     for (int row=1;row<=num;row++)
     {
-        for(int col=row;col<num+row;col++)
+        for(int col=row;col<row+row;col++)
         {
             printf("%d",col);
         }
