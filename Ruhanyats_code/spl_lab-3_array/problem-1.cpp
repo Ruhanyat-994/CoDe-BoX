7fe22c06c10360b78fce97e8471b4ac276faefeb
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-04-15 20:52:37 +0600

    Automated commit for programming in ./spl_lab-3_array

--- Ruhanyats_code/spl_lab-3_array/problem-1.cpp
+++ Ruhanyats_code/spl_lab-3_array/problem-1.cpp
@@ -1,1 +1,19 @@
 
+#include<stdio.h>
+int main()
+{
+    int num1;
+    scanf("%d",&num1);
+    int arr[num1];
+
+    for(int i = 0;i<num1;i++)
+    {
+        scanf("%d",&arr[i]);
+
+    }
+    for(int i=num1-1;i>=0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
