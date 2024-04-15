e45ba4334b460e7182ec3f6ca4659312d390b1e7
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-04-15 23:51:05 +0600

    Automated commit for programming in ./spl_lab-3_array

---
+++ Ruhanyats_code/spl_lab-3_array/problem-5.cpp
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    int sum=0;
+    scanf("%d",&num);
+
+    int arr[num];
+
+    //this for loop is for looping to num and taking the other numbers as input
+    for(int i = 0 ; i<num;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i=num-1;i>=0;i--)
+    {
+        if (i%2==0)
+        {
+            sum = sum + arr[i];
+        }
+
+    }
+    printf("%d",sum);
+
+}
+
+
+
